7c297230dc497248cca621705ec655ff38616ff2
MrJeleika <den932005@gmail.com>
2024-12-12 22:21:38 +0200

    fix

--- src/app/hooks/get/use-managers.ts
+++ src/app/hooks/get/use-managers.ts
@@ -2,8 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 
 import { get } from '../utils';
 
-import { apiClient } from '@/lib/fetcher';
 import { Employee } from '@/app/database/entities/employee.entity';
+import { apiClient } from '@/lib/fetcher';
 
 export const useManagers = () => {
   return useQuery({
